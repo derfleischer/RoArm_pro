4a92d314d448971326b2229d11dc7b6bd6b5edb5
derfleischer <77880290+derfleischer@users.noreply.github.com>
2025-08-06 09:38:58 +0200

    Delete roarm_pro directory

--- roarm_pro/roarm_pro/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Configuration module for RoArm Pro"""
-
-from .defaults import *
-from .settings import Settings
-
-__all__ = [
-    'SERVO_LIMITS', 'SCANNER_LIMITS', 'HOME_POSITION',
-    'COMMANDS', 'CALIBRATION_POSITIONS', 'Settings'
-]
--- roarm_pro/roarm_pro/ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""User interface module"""
-
-from .app import RoArmApp
-from .menus import MenuSystem
-
-__all__ = ['RoArmApp', 'MenuSystem']
